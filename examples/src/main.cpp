ac643ae3640ea9c0b1d21b35d4b6d2b33d8313c5
RomanCizmarik <roman.cizmarik@gmail.com>
2026-02-06 11:01:04 +0100

    Simple example

--- examples/src/main.cpp
+++ examples/src/main.cpp
@@ -132,19 +132,142 @@ void FWNSpeedTest(DMB::SimpleMesh& m)
 }
 
 
+//int main(int argc, char** argv)
+//{
+//    //for (auto fn : { "C:/skola/PhD/Samples/booleans/sphere_9x6.obj" , "C:/skola/PhD/VUT/booleans_paper/evaluation/Validity/input_models/jaw.obj" })
+//    //{
+//    //    std::cout << fn << std::endl;
+//
+//    //    DMB::SimpleMesh m;
+//    //    OpenMesh::IO::read_mesh(m, fn);
+//    //    FWNTreeTest(m);
+//    //    FWNSpeedTest(m);
+//    //}
+//
+//    //DMB::SimpleMesh m;
+//    //DMB::meshUnion(m, "C:/skola/PhD/VUT/booleans_paper/evaluation/Validity/input_models/101954.stl", "C:/skola/PhD/VUT/booleans_paper/evaluation/Validity/input_models/894835.stl");
+//    //OpenMesh::IO::write_mesh(m, "out.obj");
+//
+//
+//
+//    return 0;
+//}
+
+
+bool printHelp(const std::map<std::string, std::string>& arguments)
+{
+    if (arguments.find("h") != arguments.end() || arguments.find("help") != arguments.end())
+    {
+        std::cout <<
+            "This executable is ment for running an algorithm as a separate process. \n"
+            "To run an algorithm, share your data via shared memory and specify the algorithm to run using -algorithm parameter. \n"
+            "\n"
+            "Parameters: \n"
+            "-algorithm:NAME_OF_ALGORITHM_TO_RUN    Runs algorithm specified by the string NAME_OF_ALGORITHM_TO_RUN. \n"
+            "-h                                     Shows this help. \n"
+            "-help                                  Shows this help. \n"
+            "Example usage: \n"
+            "./BSPWorker.exe -algorithm:BooleanUnion"
+            << std::endl;
+        return true;
+    }
+
+    return false;
+}
+
+void parseArguments(int argc, char** argv, std::map<std::string, std::string>& arguments, std::vector<std::string>& unnamedArguments)
+{
+    const std::string delimiter = ":";
+    const std::string prefix = "-";
+
+    for (int a = 1; a < argc; ++a)
+    {
+        std::string s(argv[a]);
+
+        //unnamed arguments - not starting with prefix
+        if (s.substr(0, 1) != prefix)
+        {
+            unnamedArguments.push_back(s);
+            continue;
+        }
+
+        auto it = s.find(delimiter);
+        std::string key = s.substr(1, it - 1); //get rid of prefix
+        std::string value = s.substr(it + 1, s.size()); //get rid of delimiter
+
+        arguments[key] = value;
+    }
+}
+
+bool runAlgorithm(const std::map<std::string, std::string>& arguments, const std::vector<std::string>& unnamedArguments)
+{
+    DMB::SimpleMesh result;
+    bool success = false;
+
+    if (unnamedArguments[0] == "U")
+    {
+        success = DMB::meshUnion(result, unnamedArguments[1], unnamedArguments[2]);
+    }
+    else if (unnamedArguments[0] == "D")
+    {
+        success = DMB::meshSubtraction(result, unnamedArguments[1], unnamedArguments[2]);
+    }
+    else if (unnamedArguments[0] == "I")
+    {
+        success = DMB::meshIntersection(result, unnamedArguments[1], unnamedArguments[2]);
+    }
+    else
+    {
+        return false;
+    }
+
+    if (success && unnamedArguments.size() > 3)
+    {
+        try
+        {
+            OpenMesh::IO::write_mesh(result, unnamedArguments[3]);
+        }
+        catch (...)
+        {
+            std::cout << "writing failed" << std::endl;
+
+        }
+    }
+
+    return success;
+}
+
 int main(int argc, char** argv)
 {
-    //for (auto fn : { "C:/skola/PhD/Samples/booleans/sphere_9x6.obj" , "C:/skola/PhD/VUT/booleans_paper/evaluation/Validity/input_models/jaw.obj" })
-    //{
-    //    std::cout << fn << std::endl;
+    std::map<std::string, std::string> namedArguments;
+    std::vector<std::string> unnamedArguments;
 
-    //    DMB::SimpleMesh m;
-    //    OpenMesh::IO::read_mesh(m, fn);
-    //    FWNTreeTest(m);
-    //    FWNSpeedTest(m);
-    //}
+    parseArguments(argc, argv, namedArguments, unnamedArguments);
 
-    DMB::SimpleMesh m;
-    DMB::meshUnion(m, "C:/skola/PhD/VUT/booleans_paper/evaluation/Validity/input_models/101954.stl", "C:/skola/PhD/VUT/booleans_paper/evaluation/Validity/input_models/894835.stl");
-    OpenMesh::IO::write_mesh(m, "out.obj");
+    if (printHelp(namedArguments))
+    {
+        return 0;
+    }
+
+    try
+    {
+        if (!runAlgorithm(namedArguments, unnamedArguments))
+        {
+            return 1;
+        }
+
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Fatal exception: " << e.what() << std::endl;
+        return 1;
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown exception" << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
+
